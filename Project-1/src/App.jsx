00b6dc1ecea3d84ef357792dc5cff067e3e91769
vikystudy34 <vikystudy34@gmail.com>
2026-01-10 10:25:40 +0530

    edit h4

--- Project-1/src/App.jsx
+++ Project-1/src/App.jsx
@@ -6,47 +6,47 @@ function App() {
 
     <div className="container">
       <div class="row mb">
-    <div class="col-6">
-      <input type="text" placeholder="Enter Username" />
-    </div>
-    <div class="col-6">
-      <input type="date" placeholder="dd-mm-yyyy" />
-    </div>
-  </div>
-  <div class="row mb">
-    <div class="col-6">
-      <input type="password" placeholder="Enter Your Password" />
-    </div>
-    <div class="col">
-      <input type="email" placeholder="Enter Your Email" />
-    </div>
-   </div>
+        <div class="col-6">
+          <input type="text" placeholder="Enter Username" />
+        </div>
+        <div class="col-6">
+          <input type="date" placeholder="dd-mm-yyyy" />
+        </div>
+      </div>
+      <div class="row mb">
+        <div class="col-6">
+          <input type="password" placeholder="Enter Your Password" />
+        </div>
+        <div class="col">
+          <input type="email" placeholder="Enter Your Email" />
+        </div>
+      </div>
 
-  <div class="row mb">
-    <div class="col-6">
-      <input type="number" placeholder="Phone Number" />
-    </div>
-    <div class="col-6">
-      <input type="text" placeholder="Enter Your Address" />
-    </div>
-  </div>
+      <div class="row mb">
+        <div class="col-6">
+          <input type="number" placeholder="Phone Number" />
+        </div>
+        <div class="col-6">
+          <input type="text" placeholder="Enter Your Address" />
+        </div>
+      </div>
 
-  <div class="row mb">
-    <div class="col-6 one">
-      <button type="button" class="btn btn-success button">Login</button>
-    </div>
-    <div class="col-4 one">
-      <button type="button" class="btn btn-danger button">Reset</button>
-    </div>
-  </div>
+      <div class="row mb">
+        <div class="col-6 one">
+          <button type="button" class="btn btn-success button">Login</button>
+        </div>
+        <div class="col-4 one">
+          <button type="button" class="btn btn-danger button">Reset</button>
+        </div>
+      </div>
 
-  <p>This Application is created by Vicky Kumar patel</p>
+      <p>This Application is created by Vicky Kumar patel</p>
     </div>
-    
+
     <h4 className="golu">
-      <p>Golu bhaiya kaisa hai mera fruntend project abhi Esme data entry karne ke bad login pr click karne pr kuch kam nahi karega Kyoki abhi kuch bhi data database me add nahi hai ye bus login dashboard ka fruntend hai</p>
+      <p>bhaiya kaisa hai mera fruntend project abhi Esme data entry karne ke bad login pr click karne pr kuch kam nahi karega Kyoki abhi kuch bhi data database me add nahi hai ye bus login dashboard ka fruntend hai</p>
     </h4>
-   
+
   </center>
 }
 
